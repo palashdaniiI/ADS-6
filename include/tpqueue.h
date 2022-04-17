464725a19edf7fba3edcdb600bf2ed76aea69ccc
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-04-17 21:59:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,21 +3,21 @@
 
 template<typename T, int size>
 class TPQueue {
- private:
- T arr[5];
+  private:
+  T arr[5];
     int start, end;
- public:
+   public:
     TPQueue() : start(0), end(0) {}
     void push(T value) {
-        int x = end;
-        while (--x >= start && value.prior > (*(arr + x % size)).prior)
- *(arr + (x + 1) % size) = *(arr + x % size);
- *(arr + (x + 1) % size) = value;
- end++;
-    }
- T pop() {
-        return *(arr + (start++) % size);
-    }
+      int x = end;
+      while (--x >= start && value.prior > (*(arr + x % size)).prior)
+        *(arr + (x + 1) % size) = *(arr + x % size);
+      *(arr + (x + 1) % size) = value;
+      end++;
+      }
+  T pop() {
+    return *(arr + (start++) % size);
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
