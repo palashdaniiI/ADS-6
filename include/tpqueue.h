46b1a81fa90275e7f1cd738b7b502ccf2f355c15
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-04-17 22:02:41 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,13 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T, int size>
 class TPQueue {
-  private:
+ private:
   T arr[5];
     int start, end;
-   public:
+ public:
     TPQueue() : start(0), end(0) {}
     void push(T value) {
       int x = end;
